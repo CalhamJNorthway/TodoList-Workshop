02a55a89b79c9d3240e8c6251fb4693757e596e7
Calham Jackson Northway <39806672+CalhamJNorthway@users.noreply.github.com>
2021-02-13 09:21:43 -0800

    Add cookie manager and use effect to manage cookies and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { MainPage } from './pages/mainPage/MainPage';
-import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Dictionary, ListItem } from './core/Types';
 import { Helpers } from './core/Helpers';
 import { DetailsPage } from './pages/detailsPage/DetailsPage';
+import {CookieKeys, CookieManager} from "./core/storage";
+import Cookies from 'universal-cookie';
 
 function App() {
   const [itemList, setItemList] = useState<Dictionary<ListItem>>({})
+
+  const cookieManager = new CookieManager(new Cookies());
+
+  useEffect(() => {
+    if (Object.keys(itemList).length === 0) {
+      const taskList = cookieManager.getByKey<Dictionary<ListItem>>(CookieKeys.TASK_LIST);
+      setItemList(taskList);
+    }
+
+    if (Object.keys(itemList).length > 0) {
+      cookieManager.set<Dictionary<ListItem>>(CookieKeys.TASK_LIST, itemList);
+    }
+  });
 
   const addItem = (item: ListItem) => {
     const key: string = Helpers.generateKey();
@@ -21,7 +36,6 @@ function App() {
     setItemList(listCopy);
   }
 
-
   const removeItem = (item: ListItem): void => {
     const listCopy: Dictionary<ListItem> = { ...itemList };
     delete listCopy[item.key];
@@ -31,7 +45,6 @@ function App() {
   const editItem = (item: ListItem): void => {
     const listCopy: Dictionary<ListItem> = { ...itemList };
     listCopy[item.key] = { ...item };
-
     setItemList(listCopy);
   }
 
